4534770b37ef572da719dc5bb7384d84ea7ed7b6
pastuhovoa-yandex <120190672+pastuhovoa-yandex@users.noreply.github.com>
2022-12-09 17:41:32 +0300

    Added new line

--- program.py
+++ program.py
@@ -2,7 +2,7 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('we need more prints')
-
+    print('UFO came and added this line')
 
 if __name__ == '__main__':
     main()
